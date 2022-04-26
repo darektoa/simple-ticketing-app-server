63c6edb8d809fede114e783dfa9ab16118823735
darektoa <darektoa@gmail.com>
2022-04-27 05:03:22 +0700

    Add: Id_card_type field in users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('phone', 20);
             $table->date('birth_date');
             $table->smallInteger('gender');
+            $table->smallInteger('id_card_type');
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
