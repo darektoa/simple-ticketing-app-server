e6d84c787e6d57001d484e0ee4310b0f678a3de1
darektoa <darektoa@gmail.com>
2022-04-28 05:05:36 +0700

    Add: User relation method in emergency contact model

--- app/Models/EmergencyContact.php
+++ app/Models/EmergencyContact.php
@@ -10,4 +10,9 @@ class EmergencyContact extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
