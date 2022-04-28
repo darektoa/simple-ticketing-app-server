d2431fbbb0577431aaadd1e52029bdfcb644b8bc
darektoa <darektoa@gmail.com>
2022-04-29 05:04:42 +0700

    Add: Fields in destinations table migration

--- database/migrations/2022_05_07_125249_create_destinations_table.php
+++ database/migrations/2022_05_07_125249_create_destinations_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('destinations', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->bigInteger('price');
+            $table->date('start_at');
+            $table->date('end_at');
             $table->timestamps();
         });
     }
