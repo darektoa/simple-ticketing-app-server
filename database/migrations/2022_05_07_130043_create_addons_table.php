86c662e923cf14f519174922ac74bab6e76c0c9c
darektoa <darektoa@gmail.com>
2022-04-30 05:04:53 +0700

    Add: Deleted_at field in addons table migration

--- database/migrations/2022_05_07_130043_create_addons_table.php
+++ database/migrations/2022_05_07_130043_create_addons_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 100);
             $table->bigInteger('price');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
