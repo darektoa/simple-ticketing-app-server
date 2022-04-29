50c46943d2b0d12090d7a417114315c038efd20e
darektoa <darektoa@gmail.com>
2022-04-30 05:02:21 +0700

    Add: Fields in addons table migration

--- database/migrations/2022_05_07_130043_create_addons_table.php
+++ database/migrations/2022_05_07_130043_create_addons_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('addons', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->bigInteger('price');
             $table->timestamps();
         });
     }
