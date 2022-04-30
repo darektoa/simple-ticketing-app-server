745c66106b169130d177c454ec57c3e28ca88954
darektoa <darektoa@gmail.com>
2022-05-01 05:02:09 +0700

    Add: Constrained method at user_id field in emergency_contacts table migration

--- database/migrations/2022_05_07_120940_create_emergency_contacts_table.php
+++ database/migrations/2022_05_07_120940_create_emergency_contacts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('emergency_contacts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained();
             $table->string('name', 40);
             $table->string('phone', 20);
             $table->softDeletes();
