07aacf50da280614953006244b4e7d036af3a5f5
darektoa <darektoa@gmail.com>
2022-05-02 05:05:23 +0700

    Add: Transaction relation method in transaction addon model

--- app/Models/TransactionAddon.php
+++ app/Models/TransactionAddon.php
@@ -10,4 +10,9 @@ class TransactionAddon extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+
+    public function transaction() {
+        return $this->belongsTo(Transaction::class);
+    }
 }
