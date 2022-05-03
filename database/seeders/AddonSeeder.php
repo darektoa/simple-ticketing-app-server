056e507f29acee2690275e82b393bf7571eec950
darektoa <darektoa@gmail.com>
2022-05-04 05:06:47 +0700

    Add: Dummy data in addon seeder

--- database/seeders/AddonSeeder.php
+++ database/seeders/AddonSeeder.php
@@ -2,18 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Addon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class AddonSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        $addons = collect([
+            ['Vest (size: S)', 150000],
+            ['Vest (size: M)', 150000],
+            ['Vest (size: L)', 150000],
+            ['Vest (size: XL)', 150000],
+        ]);
+        
+
+        $addons->each(fn($item) => Addon::create([
+            'name'  => $item[0],
+            'price' => $item[1],
+        ]));
     }
 }
