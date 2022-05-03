d4eb951d45b7a0fb46c01929267bf9ddd54dcb57
darektoa <darektoa@gmail.com>
2022-05-04 05:03:24 +0700

    Add: Paginate method in collection helper

--- app/Helpers/CollectionHelper.php
+++ app/Helpers/CollectionHelper.php
@@ -2,6 +2,31 @@
 
 namespace App\Helpers;
 
+use Illuminate\Pagination\{LengthAwarePaginator, Paginator};
+use Illuminate\Support\Collection;
+
 class CollectionHelper{
-    
+    static public function paginate(
+        $items,
+        int $perPage=15,
+        string $pageName='page',
+        int $page=null
+    ) {
+        $page       = $page ?: Paginator::resolveCurrentPage();
+        $options    = [
+            'path'      => Paginator::resolveCurrentPath(),
+            'pageName'  => $pageName,
+        ];
+        
+        if(!($items instanceof Collection))
+            $items = Collection::make($items);
+        
+        return new LengthAwarePaginator(
+            $items->skip(($page-1) * $perPage)->take($perPage),
+            $items->count(),
+            $perPage,
+            $page,
+            $options,
+        );
+    }
 }
