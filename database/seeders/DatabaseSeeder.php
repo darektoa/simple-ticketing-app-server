783ffb0d0b0677d5efa0d9c3915d1a197f9b84cc
darektoa <darektoa@gmail.com>
2022-05-05 05:02:58 +0700

    Add: Call addon seeder in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class,
+            AddonSeeder::class,
+            // UserSeeder::class,
         ]);
 
         // User::factory(20)
