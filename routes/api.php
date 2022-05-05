221827cffd53628c268853600f8d2b7646e908fe
darektoa <darektoa@gmail.com>
2022-05-06 05:05:14 +0700

    Add: Addon list route in API routes

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,5 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::get('/addons', [AddonController::class, 'index']);
