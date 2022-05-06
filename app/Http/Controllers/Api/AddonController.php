981b448de5c0e3eaa28b3dab68386ac750faaad8
darektoa <darektoa@gmail.com>
2022-05-07 05:03:38 +0700

    Add: Using addon API resource in index method of addon API controller

--- app/Http/Controllers/Api/AddonController.php
+++ app/Http/Controllers/Api/AddonController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AddonResource;
 use App\Models\Destination;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,8 @@ class AddonController extends Controller
     public function index() {
         $destinations = Destination::all();
 
-        return ResponseHelper::make($destinations);
+        return ResponseHelper::make(
+            AddonResource::collection($destinations)
+        );
     }
 }
