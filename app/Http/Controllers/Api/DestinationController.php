8603503c6cef58a7a8e29a9d19aeb7836c4a32e6
darektoa <darektoa@gmail.com>
2022-05-08 05:04:39 +0700

    Add: Index method in destination API controller

--- app/Http/Controllers/Api/DestinationController.php
+++ app/Http/Controllers/Api/DestinationController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Models\Destination;
 use Illuminate\Http\Request;
 
 class DestinationController extends Controller
 {
-    //
+    public function index() {
+        $destinations = Destination::all();
+
+        return ResponseHelper::make($destinations);
+    }
 }
