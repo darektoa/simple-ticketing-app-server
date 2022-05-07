87eb5ab7829f8ca4aab0f89c85d2693f85be1d9a
darektoa <darektoa@gmail.com>
2022-05-08 05:06:11 +0700

    Fix: Destination API resource class name

--- app/Http/Resources/DestinationResource.php
+++ app/Http/Resources/DestinationResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class AddonResource extends JsonResource
+class DestinationResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
