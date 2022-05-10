5ec74090d73d24f4aed8c2ecdfb53f1cc4e6bf33
darektoa <darektoa@gmail.com>
2022-05-11 05:02:18 +0700

    Add: Constructor in error exception class

--- app/Exceptions/ErrorException.php
+++ app/Exceptions/ErrorException.php
@@ -3,8 +3,26 @@
 namespace App\Exceptions;
 
 use Exception;
+use Throwable;
+use Illuminate\Support\Collection;
 
 class ErrorException extends Exception
 {
-	
+	private $errors;
+
+
+	public function __construct(
+		string $message = 'Failed',
+		int $code = 400,
+		array|Collection $errors = [],
+		?Throwable $previous = null,
+	) {
+		parent::__construct($message, $code, $previous);
+		$this->errors = $errors;
+	}
+
+
+	public function getErrors() {
+		return $this->errors;
+	}
 }
