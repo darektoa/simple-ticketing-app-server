db16fbea18d283e54cb16a47de8698ec604cad73
darektoa <darektoa@gmail.com>
2022-05-13 05:02:37 +0700

    Add: Detail cast to json type in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
 
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'detail'            => 'json',
     ];
     
     protected $guarded = ['id'];
