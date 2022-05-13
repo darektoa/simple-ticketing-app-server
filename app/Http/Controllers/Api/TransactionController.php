4078283b1569cae74ed6ce4dd0d910f8e373fff6
darektoa <darektoa@gmail.com>
2022-05-14 05:05:12 +0700

    Add: Index method in transaction API controller

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -2,10 +2,89 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\ErrorException as Error;
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TransactionResource;
+use App\Models\Addon;
+use App\Models\Destination;
+use App\Models\User;
 use App\Traits\Api\RequestValidator;
+use App\Traits\XenditTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class TransactionController extends Controller
 {
     use RequestValidator;
+
+    public function store(Request $request) {
+        try{
+            $this->validate($request, [
+                'destination_id'    => 'required|exists:destinations,id',
+                'addons_id'         => 'nullable|exists:addons,id',
+                'first_name'        => 'required|min:2|max:20',
+                'last_name'         => 'required|min:2|max:20',
+                'email'             => 'required|email|unique:users,email',
+                'phone'             => 'required|min:10|max:20',
+                'birth_date'        => 'required|date',
+                'gender'            => 'required|in:1,2',
+                'id_card_type'      => 'required|min:3|max:20',
+                'id_card_number'    => 'required|min:6|max:20',
+                'blood_type'        => 'nullable|min:1|max:3',
+            ]);
+            
+            // CREATE USER
+            $user = User::create([
+                'first_name'    => $request->first_name,
+                'last_name'     => $request->last_name,
+                'email'         => $request->email,
+                'password'      => Hash::make('password123'),
+                'phone'         => $request->phone,
+                'birth_date'    => $request->birth_date,
+                'gender'        => $request->gender,
+            ]);
+
+            $user->update([
+                'detail' => collect($request)->except(
+                    collect($user)->keys()
+                )
+            ]);
+
+            // CREATE TRANSACTION
+            $destination = Destination::find($request->destination_id);
+            $addon       = Addon::find($request->addons_id ?? null);
+            $transaction = $user->receiveTransactions()->create([
+                'amount'    => 250000,
+                'type'      => 2,
+            ]);
+ 
+            if($destination) $transaction->destination()->create([
+                'destination_id'    => $destination->id,
+                'price'             => $destination->price,
+            ]);
+            
+            if($addon) $transaction->addon()->create([
+                'addon_id'  => $addon->id,
+                'price'     => $addon->price,
+            ]);
+
+            $invoice = XenditTrait::invoice($transaction);
+            $transaction->update([
+                'detail'    => ['invoices' => [
+                    'pending'=> $invoice,
+                ]],
+            ]);
+
+            return ResponseHelper::make(
+                TransactionResource::make($transaction->load(['receiver', 'destination', 'addon']))
+            );
+        }catch(Error $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
+    }
 }
