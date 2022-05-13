2df1635fe74fb591a1ce3ccfe917995133529d06
darektoa <darektoa@gmail.com>
2022-05-14 05:02:05 +0700

    Add: Properties in user API resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,19 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'            => $this->id,
+            'first_name'    => $this->first_name,
+            'last_name'     => $this->last_name,
+            'email'         => $this->email,
+            'phone'         => $this->phone,
+            'birth_date'    => $this->birth_date,
+            'gender'        => [
+                'id'    => $this->gender,
+                'name'  => $this->gender_name,
+            ],
+            'created_at'    => $this->created_at,
+            'detail'        => $this->detail,
+        ];
     }
 }
