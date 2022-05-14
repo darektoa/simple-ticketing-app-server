325ff60afa5df7c022769212e34617b641c2f83a
darektoa <darektoa@gmail.com>
2022-05-15 05:02:23 +0700

    Add: Using prefix method at tickets route in API routes

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/addons', [AddonController::class, 'index']);
 Route::get('/destinations', [DestinationController::class, 'index']);
 
-Route::post('/tickets', [TransactionController::class, 'store']);
+Route::prefix('tickets')->group(function() {
+    Route::post('/', [TransactionController::class, 'store']);
+});
