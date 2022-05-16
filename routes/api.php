c491a9b7d2a8310de094c3579eff57538c41287b
darektoa <darektoa@gmail.com>
2022-05-17 05:02:56 +0700

    Add: Xendit callback route in API routes

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/addons', [AddonController::class, 'index']);
 Route::get('/destinations', [DestinationController::class, 'index']);
 
-Route::prefix('tickets')->group(function() {
+Route::prefix('/tickets')->group(function() {
     Route::post('/', [TransactionController::class, 'store']);
     Route::get('/{transaction:code}', [TransactionController::class, 'show']);
+});
+
+Route::prefix('/xendit')->group(function() {
+    Route::middleware(['xendit.callback'])->group(function() {
+        Route::post('/callback', [TransactionController::class, 'xenditCallback']);
+    });
 });
