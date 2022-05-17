f49cb693091ddf617f7ecee0d22ccbc2d1d47ae3
darektoa <darektoa@gmail.com>
2022-05-18 05:05:31 +0700

    Fix: User seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,9 +11,14 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name'      => 'User',
-            'email'     => 'user@example.com',
-            'password'  => Hash::make('password'),
+            'first_name'    => 'User',
+            'last_name'     => 'Example',
+            'email'         => 'user@example.com',
+            'phone'         => '000000000000',
+            'birth_date'    => '2000-01-01',
+            'gender'        => 1,
+            'role'          => 1,
+            'password'      => Hash::make('password'),
         ]);
     }
 }
