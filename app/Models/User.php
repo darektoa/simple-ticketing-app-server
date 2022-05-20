9a0ae0b6015925a8d8c8c1ca98827e85ec70f755
darektoa <darektoa@gmail.com>
2022-05-21 05:04:22 +0700

    Add: Append full_name property in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable, Searchable, SoftDeletes;
 
     protected $appends = [
-        'gender_name'
+        'gender_name', 'full_name'
     ];
 
     protected $casts = [
