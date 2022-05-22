69d2809d6e677f37543ce51aae06a90cf21c5c4b
darektoa <darektoa@gmail.com>
2022-05-23 05:02:04 +0700

    Edit: Return view to success register mail blade view in build method of success register mail class

--- app/Mail/SuccessRegisterMail.php
+++ app/Mail/SuccessRegisterMail.php
@@ -28,6 +28,6 @@ class SuccessRegisterMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.succes-register');
     }
 }
