742fcc0a1c59369f67cb524494bab2f2a217ca88
darektoa <darektoa@gmail.com>
2022-05-24 05:06:23 +0700

    Add: Data property in success paid mail class

--- app/Mail/SuccessPaidMail.php
+++ app/Mail/SuccessPaidMail.php
@@ -11,14 +11,16 @@ class SuccessPaidMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $data;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($data)
     {
-        //
+        $this->data = (object) $data;
     }
 
     /**
