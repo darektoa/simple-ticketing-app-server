5c7420ee5b23bdda2445b8ba91c7f2adffe63136
darektoa <darektoa@gmail.com>
2022-05-27 05:04:23 +0700

    Add: Compact destinations variable with total registrants property in index method of dashboard web controller

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\TransactionDestination;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('pages.all.dashboard.index');
+        $destinations = TransactionDestination::with(['destination' => fn($query) => $query->select('id', 'name')])
+            ->selectRaw('destination_id, COUNT(*) AS total')
+            ->groupBy('destination_id')
+            ->get();
+
+        return view('pages.all.dashboard.index', compact('destinations'));
     }
 }
