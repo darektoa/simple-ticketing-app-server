a4a9f2a9ecaea595b306a0c0f00266c2b3100e64
darektoa <darektoa@gmail.com>
2022-05-28 05:05:32 +0700

    Edit: Align value attribute indents at info card components in dashboard view page

--- resources/views/pages/all/dashboard/index.blade.php
+++ resources/views/pages/all/dashboard/index.blade.php
@@ -4,9 +4,9 @@
     <x-layout.section title="Dashboard" />
     
     <x-view.row>
-        <x-card.info title="Total Registrants" :value="Transaction::count()" icon="fa-users text-primary" />
+        <x-card.info title="Total Registrants"    :value="Transaction::count()" icon="fa-users text-primary" />
         <x-card.info title="Pending Transactions" :value="Transaction::where('status', 1)->count()" icon="fa-coins text-gray-300" color="warning" />
-        <x-card.info title="Paid Transactions" :value="Transaction::where('status', 2)->count()" icon="fa-cart-shopping text-gray-300" color="success" />
+        <x-card.info title="Paid Transactions"    :value="Transaction::where('status', 2)->count()" icon="fa-cart-shopping text-gray-300" color="success" />
         <x-card.info title="Expired Transactions" :value="Transaction::where('status', 4)->count()" icon="fa-clock-rotate-left text-gray-300" color="danger" />
     </x-view.row>
 
